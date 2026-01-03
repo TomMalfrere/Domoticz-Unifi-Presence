c74e2a0c4cc0f4c6c1def6cb9efcc16e96cf527a
TomMalfrere <35227756+TomMalfrere@users.noreply.github.com>
2026-01-04 00:57:04 +0100

    cleanup fakeDomoticz.py

--- tests/fakeDomoticz.py
+++ tests/fakeDomoticz.py
@@ -1,16 +1,7 @@
 #
-#   Buienradar.nl Weather Lookup Plugin
-#
-#   Frank Fesevur, 2017
-#   https://github.com/ffes/domoticz-buienradar
-#
-#   About the weather service:
-#   https://www.buienradar.nl/overbuienradar/gratis-weerdata
-#
 #   Very simple module to make local testing easier
 #   It "emulates" Domoticz.Log() and Domoticz.Debug()
 #
-#from _typeshed import FileDescriptor
 from typing import Any, Dict
 
 
